abbae94963138d2c228bcac0ce1b401a0a810d52
PranabM <drew.pranab7@gmail.com>
2023-02-09 16:30:48 -0700

    started again

--- Backend/src/main/java/com/serverApp/stockAlertApp/StockAlertAppApplication.java
+++ Backend/src/main/java/com/serverApp/stockAlertApp/StockAlertAppApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.serverApp.stockAlertApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class StockAlertAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(StockAlertAppApplication.class, args);
 	}
 
 }
